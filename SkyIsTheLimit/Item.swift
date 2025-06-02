92346b139bd8a013934071676149d3862c0342bf
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-06-02 06:30:42 +0200

    init project

---
+++ SkyIsTheLimit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SkyIsTheLimit
+//
+//  Created by Youngjoon Lee on 6/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SkyIsTheLimitTests/SkyIsTheLimitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkyIsTheLimitTests.swift
+//  SkyIsTheLimitTests
+//
+//  Created by Youngjoon Lee on 6/2/25.
+//
+
+import Testing
+@testable import SkyIsTheLimit
+
+struct SkyIsTheLimitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
