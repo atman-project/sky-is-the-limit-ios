63d1d628c374a03bd77d34c06045805398a491cc
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-06-15 17:28:12 +0900

    fix: add id for flight

--- SkyIsTheLimit/Models/Flight.swift
+++ SkyIsTheLimit/Models/Flight.swift
@@ -9,7 +9,8 @@ import Foundation
 import SwiftData
 
 @Model
-final class Flight {
+final class Flight: Identifiable {
+    var id: UUID
     var departure_airport: String
     var arrival_airport: String
     var departure_localtime: Date
@@ -19,7 +20,8 @@ final class Flight {
     var flight_number: String
     var booking_reference: String
 
-    init(departure_airport: String, arrival_airport: String, departure_localtime: Date, arrival_localtime: Date, airline: String, aircraft: String, flight_number: String, booking_reference: String) {
+    init(id: UUID = UUID(), departure_airport: String, arrival_airport: String, departure_localtime: Date, arrival_localtime: Date, airline: String, aircraft: String, flight_number: String, booking_reference: String) {
+        self.id = id
         self.departure_airport = departure_airport
         self.arrival_airport = arrival_airport
         self.departure_localtime = departure_localtime
